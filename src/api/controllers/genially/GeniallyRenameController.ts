a59db9b18b416cde744d0954ea80b4c8bbad4d2c
Santiago Correa <scorrea153@gmail.com>
2021-11-04 18:53:59 +0100

    clean

--- src/api/controllers/genially/GeniallyRenameController.ts
+++ src/api/controllers/genially/GeniallyRenameController.ts
@@ -7,25 +7,6 @@ import { inject, injectable } from "inversify";
 
 const service = new RenameGeniallyService(new InMemoryGeniallyRepository());
 
-/*export default async (req: Request, res: Response) => {
-    const geniallyId = req.params.id;
-    const newName = req.body.name;
-
-    try {
-        await service.execute(geniallyId, newName);
-        res.status(204).send();
-    } catch (error) {
-        switch (error.constructor) {
-            case GeniallyNotExist:
-                res.status(404).send();
-                break;
-            case GeniallyNameInvalidLength:
-                res.status(400).send({ error: error.message });
-                break;
-        }
-    }
-};*/
-
 @injectable()
 export default class GeniallyRenameController {
     constructor(@inject("RenameGeniallyService") private service: RenameGeniallyService) { }
